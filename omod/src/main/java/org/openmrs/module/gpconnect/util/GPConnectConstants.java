8858caba3ef0e4db16127f02a1fe7b47d36af595
Mircea M <mircea.moise@gmail.com>
2020-04-27 17:15:25 +0100

    Mircea: adding the authorization filter to enable access to resources

--- omod/src/main/java/org/openmrs/module/gpconnect/util/GPConnectConstants.java
+++ omod/src/main/java/org/openmrs/module/gpconnect/util/GPConnectConstants.java
@@ -1,7 +1,8 @@
 package org.openmrs.module.gpconnect.util;
 
 public final class GPConnectConstants {
-
-    public static final String GPCONNECT_SERVER_NAME = "OpenMRS GPConnect Server";
-    public static final String GPCONNECT_SERVER_DES = "OpenMRS GPConnect API implementation";
+	
+	public static final String GPCONNECT_SERVER_NAME = "OpenMRS GPConnect Server";
+	
+	public static final String GPCONNECT_SERVER_DES = "OpenMRS GPConnect API implementation";
 }
