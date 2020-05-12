4f9922c1a1d49acc89b14fd58fc3f5faa607cc13
Mircea M <mircea.moise@gmail.com>
2020-05-12 13:36:42 +0100

    Mircea: Quick naive fix for the test - it needs to be enhanced when the actual functionality is added

--- omod/src/test/java/org/openmrs/module/gpconnect/strategy/GPConnectPatientStrategyTest.java
+++ omod/src/test/java/org/openmrs/module/gpconnect/strategy/GPConnectPatientStrategyTest.java
@@ -1,16 +1,40 @@
 package org.openmrs.module.gpconnect.strategy;
 
 import org.hl7.fhir.dstu3.model.Patient;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.ServiceContext;
+import org.openmrs.module.gpconnect.entity.Person;
+import org.openmrs.module.gpconnect.repository.NhsPatientRepository;
+import org.openmrs.module.gpconnect.repository.PersonRepository;
+
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class GPConnectPatientStrategyTest {
+	
+	@Mock
+	PersonRepository personRepository;
+	
+	@Mock
+	NhsPatientRepository nhsPatientRepository;
+	
+	@InjectMocks
+	GPConnectPatientStrategy patientStrategy;
+	
+	@Before
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
 	
 	@Test
 	public void shouldMapFakeEmail() {
@@ -21,7 +45,9 @@ public class GPConnectPatientStrategyTest {
 		when(mockPatientService.getPatientByUuid(patientUuid)).thenReturn(new org.openmrs.Patient());
 		serviceContext.setPatientService(mockPatientService);
 		Context.setContext(serviceContext);
-		GPConnectPatientStrategy patientStrategy = new GPConnectPatientStrategy();
+		Person person = new Person();
+		when(personRepository.findByUuid(any())).thenReturn(Optional.of(person));
+		when(nhsPatientRepository.findById(any())).thenReturn(Optional.empty());
 		
 		Patient test = patientStrategy.getPatient(patientUuid);
 		
