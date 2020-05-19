04bcaf80413f1122aae0cc30439b3c961756cf52
Mircea M <mircea.moise@gmail.com>
2020-05-19 16:29:14 +0100

    Mircea: Re-implementing reading of cadaveric donor using DAO approach

---
+++ omod/src/main/java/org/openmrs/module/gpconnect/dao/NhsPatientDao.java
@@ -0,0 +1,22 @@
+package org.openmrs.module.gpconnect.dao;
+
+import org.hibernate.SessionFactory;
+import org.openmrs.module.gpconnect.entity.NhsPatient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import static org.hibernate.criterion.Restrictions.eq;
+
+@Component
+public class NhsPatientDao {
+	
+	@Autowired
+	@Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
+	
+	public NhsPatient getPatientById(Long id) {
+		return (NhsPatient) sessionFactory.getCurrentSession().createCriteria(NhsPatient.class).add(eq("id", id))
+		        .uniqueResult();
+	}
+}
---
+++ omod/src/main/java/org/openmrs/module/gpconnect/services/NhsPatientService.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.gpconnect.services;
+
+import org.openmrs.module.gpconnect.dao.NhsPatientDao;
+import org.openmrs.module.gpconnect.entity.NhsPatient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NhsPatientService {
+	
+	@Autowired
+	NhsPatientDao nhsPatientDao;
+	
+	public NhsPatient findById(Long id) {
+		return nhsPatientDao.getPatientById(id);
+	}
+	
+}
