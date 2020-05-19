060f760b5550945392dbaac4857885d47b592968
Mircea M <mircea.moise@gmail.com>
2020-05-19 13:57:50 +0100

    Mircea: removing spring data so it can integrate into OpenGP distribution
    Had to go back to basics as integration into a vanilla OpenMRS distribution proved difficult for multiple reasons - most importantly the use of newer openmrs core version than everything else in the distribution

--- omod/src/main/java/org/openmrs/module/gpconnect/repository/GPConnectPatientRepository.java
+++
@@ -1,6 +0,0 @@
-package org.openmrs.module.gpconnect.repository;
-
-import org.openmrs.module.gpconnect.entity.GPConnectPatient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GPConnectPatientRepository extends CrudRepository<GPConnectPatient, Long> {}
--- omod/src/main/java/org/openmrs/module/gpconnect/strategy/GPConnectPatientStrategy.java
+++ omod/src/main/java/org/openmrs/module/gpconnect/strategy/GPConnectPatientStrategy.java
@@ -1,47 +1,22 @@
 package org.openmrs.module.gpconnect.strategy;
 
-import org.hl7.fhir.dstu3.model.BooleanType;
 import org.hl7.fhir.dstu3.model.ContactPoint;
-import org.hl7.fhir.dstu3.model.Extension;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.openmrs.module.fhir.api.strategies.patient.PatientStrategy;
-import org.openmrs.module.gpconnect.entity.NhsPatient;
-import org.openmrs.module.gpconnect.entity.Person;
-import org.openmrs.module.gpconnect.repository.NhsPatientRepository;
-import org.openmrs.module.gpconnect.repository.PersonRepository;
-import org.openmrs.module.gpconnect.util.GPConnectExtensions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component("GPConnectPatientStrategy")
 public class GPConnectPatientStrategy extends PatientStrategy {
 	
-	@Autowired
-	PersonRepository personRepository;
-	
-	@Autowired
-	NhsPatientRepository nhsPatientRepository;
-	
 	@Override
 	public Patient getPatient(String uuid) {
 		Patient patient = super.getPatient(uuid);
+		if (patient == null) {
+			return patient;
+		}
 		ContactPoint email = new ContactPoint();
 		email.setSystem(ContactPoint.ContactPointSystem.EMAIL).setValue("test@mail.com");
 		patient.addTelecom(email);
-		
-		Optional<Person> person = personRepository.findByUuid(patient.getId());
-		
-		if (person.isPresent()) {
-			Optional<NhsPatient> nhsPatient = nhsPatientRepository.findById(person.get().id);
-			
-			if (nhsPatient.isPresent()) {
-				Extension cadavericDonor = new Extension(GPConnectExtensions.CADAVERIC_DONOR_URL, new BooleanType(
-				        nhsPatient.get().cadavericDonor));
-				patient.addExtension(cadavericDonor);
-			}
-		}
 		
 		return patient;
 	}
