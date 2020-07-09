7f556c951f5b24e4776bd50b4fcdf50a3e2d0e07
Joel Walker <hello@joelwalker.co.uk>
2020-07-09 15:18:16 +0100

    Joel/Oliva: Interceptor returns bad request for invalid paramiters

--- omod/src/test/java/org/openmrs/module/gpconnect/providers/GPConnectPatientProviderWebTest.java
+++ omod/src/test/java/org/openmrs/module/gpconnect/providers/GPConnectPatientProviderWebTest.java
@@ -1,16 +1,33 @@
 package org.openmrs.module.gpconnect.providers;
 
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
-import ca.uhn.fhir.rest.param.TokenAndListParam;
-import ca.uhn.fhir.rest.param.TokenParam;
-import ca.uhn.fhir.rest.server.BundleProviders;
-import lombok.AccessLevel;
-import lombok.Getter;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
 import org.apache.commons.io.IOUtils;
-import org.hl7.fhir.dstu3.model.OperationOutcome;
 import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.DateTimeType;
+import org.hl7.fhir.dstu3.model.OperationOutcome;
+import org.hl7.fhir.dstu3.model.OperationOutcome.OperationOutcomeIssueComponent;
+import org.hl7.fhir.dstu3.model.Patient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,37 +35,21 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.GreaterOrEqual;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.module.fhir2.api.FhirPatientService;
 import org.openmrs.module.fhir2.api.dao.FhirPatientDao;
 import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
 import org.openmrs.module.gpconnect.mappers.NhsPatientMapper;
+import org.openmrs.module.gpconnect.util.CodeSystems;
 import org.openmrs.module.gpconnect.util.Extensions;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import javax.servlet.ServletException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
+import ca.uhn.fhir.rest.api.server.IBundleProvider;
+import ca.uhn.fhir.rest.param.TokenAndListParam;
+import ca.uhn.fhir.rest.param.TokenParam;
+import lombok.AccessLevel;
+import lombok.Getter;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GPConnectPatientProviderWebTest extends BaseFhirR3ResourceProviderWebTest<GPConnectPatientProvider, Patient> {
@@ -146,7 +147,6 @@ public class GPConnectPatientProviderWebTest extends BaseFhirR3ResourceProviderW
 
     @Test
     public void shouldSkipDeadPatientsInSearch() throws IOException, ServletException {
-        String nhsNumber = "1234567890";
 
         org.hl7.fhir.r4.model.Patient inactivePatient = new org.hl7.fhir.r4.model.Patient();
 
@@ -173,11 +173,6 @@ public class GPConnectPatientProviderWebTest extends BaseFhirR3ResourceProviderW
         Bundle resource = readBundleResponse(response);
 
         assertThat(resource.getEntry().size(), equalTo(0));
-        //verify(patientDao, times(1)).search(searchParameterMapArgumentCaptor.capture(), any());
-        //TokenParam tokenParam = ((TokenAndListParam) searchParameterMapArgumentCaptor.getValue().getParameters("identifier.search.handler").get(0).getParam()).getValuesAsQueryTokens().get(0).getValuesAsQueryTokens().get(0);
-        //assertThat(tokenParam.getSystem(), equalTo(Extensions.NHS_NUMBER_SYSTEM));
-        //assertThat(tokenParam.getValue(), equalTo(nhsNumber));
-
     }
 
 
@@ -185,13 +180,24 @@ public class GPConnectPatientProviderWebTest extends BaseFhirR3ResourceProviderW
     public void shouldThrowAppropreateExceptionForInvalidUrlParmiters() throws Exception {
 
         MockHttpServletResponse response = get("/Patient?Identifier=https://fhir.nhs.uk/Id/nhs-number|1234567890").go();
-
-        assertThat(response, statusEquals(400));
+        
         OperationOutcome resource = (OperationOutcome) readOperationOutcomeResponse(response);
-
+        List<OperationOutcomeIssueComponent> issues = resource.getIssue();
+        
+        assertThat(response, statusEquals(400));
         assertTrue(resource.hasMeta());
+        assertThat(issues.size(), greaterThanOrEqualTo(1));
 
-        assertThat(resource.getIssue().size(), greaterThanOrEqualTo(1));
+        for (OperationOutcomeIssueComponent issue : issues) {
+            Coding expectedCoding = new Coding(CodeSystems.SPINE_ERROR_OR_WARNING_CODE, "BAD_REQUEST", "BAD_REQUEST");
+            List<Coding> coding = issue.getDetails().getCoding();
 
+            assertEquals(issue.getSeverity(), OperationOutcome.IssueSeverity.ERROR);
+            assertTrue(issue.hasCode());
+            assertThat(coding.size(), equalTo(1));
+            assertTrue(coding.get(0).hasCode());
+            assertTrue(coding.get(0).hasDisplay());
+            assertEquals(expectedCoding.getCode(), coding.get(0).getCode());
+        }
     }
 }
