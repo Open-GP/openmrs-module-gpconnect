4b35435d22afe54e029ef59f13fac60c53cd0ab2
Olivia Frost <olivia.frost617@googlemail.com>
2020-07-28 16:50:23 +0100

    Olivia/Trystan: Create nhs_practitioner table to store sds_role_profile_id

--- api/src/main/java/org/openmrs/module/gpconnect/dao/NhsPractitionerDao.java
+++ api/src/main/java/org/openmrs/module/gpconnect/dao/NhsPractitionerDao.java
@@ -1,4 +1,27 @@
 package org.openmrs.module.gpconnect.dao;
 
+import org.hibernate.SessionFactory;
+import org.openmrs.module.gpconnect.entity.NhsPractitioner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hibernate.criterion.Restrictions.eq;
+
+@Component
+@Transactional
 public class NhsPractitionerDao {
+
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    public NhsPractitioner getPractitionerById(Long id) {
+        return (NhsPractitioner) sessionFactory.getCurrentSession().createCriteria(NhsPractitioner.class).add(eq("id", id))
+                .uniqueResult();
+    }
+
+    public void saveOrUpdate(NhsPractitioner nhsPractitioner) { sessionFactory.getCurrentSession().saveOrUpdate(nhsPractitioner); }
 }
+
